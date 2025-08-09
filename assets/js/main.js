65f13981c47add11d14a49f25dc5b39933ceab72
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-08-09 11:13:53 +0300

    Navbar sticky and scroll-up toggle on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,34 @@
 // On Scroll Function
+function navMenu() {
+    let navBar = document.querySelector('.navbar-sticky');
+    let scrollTopButton = document.querySelector('#scrollUp');
 
+    window.onscroll = function () {
+        var scroll = document.documentElement.scrollTop;
+        if(scroll >= 120) {
+            navBar.classList.add("navbar-sticky-moved-up");
+        } else {
+            navBar.classList.remove("navbar-sticky-moved-up");
+        }
+
+        // Apply Transition
+        if(scroll >= 250) {
+            navBar.classList.add("navbar-sticky-transitioned");
+            scrollTopButton.classList.add("scrollActive");
+        } else {
+            navBar.classList.remove("navbar-sticky-transitioned");
+            scrollTopButton.classList.remove("scrollActive");
+        }
+
+        // Sticky On
+        if(scroll >= 500) {
+            navBar.classList.add("navbar-sticky-on");
+        } else {
+            navBar.classList.remove("navbar-sticky-on");
+        }
+    }
+}
+navMenu();
 
 // Counter design
 
