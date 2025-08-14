8bc682582256d094ffdf956f09924cac54168b26
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-08-14 16:04:39 +0300

    Initialize app screenshots slider with coverflow

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,6 +55,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 // APP-SCREEN
-
+var swiper = new Swiper('.swiper-container.app-screen', {
+    effect: 'coverflow',
+    loop: true,
+    autoplaySpeed: 3000,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    paginationClickable: true,
+    coverflow: {
+        rotate: 0,
+        stretch: 100,
+        depth: 150,
+        modifier: 1.5,
+        slideShadows: false,
+    }
+});
 
 // Side Navigation Bar Close While We Click On Navigation Links
